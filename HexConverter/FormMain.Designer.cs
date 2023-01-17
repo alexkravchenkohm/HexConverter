288dd5ea9d16d81609f5e0a0f7ae8bcfd5a5890c
Alex Kravchenko <alex.kravchenko@hotmail.com>
2023-01-16 21:19:02 -0800

    Remove a tiny visual artifact in the 16x16 application icon

--- HexConverter/FormMain.Designer.cs
+++ HexConverter/FormMain.Designer.cs
@@ -199,7 +199,7 @@ namespace HexConverter
             this.formatDecToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("formatDecToolStripButton.Image")));
             this.formatDecToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.formatDecToolStripButton.Name = "formatDecToolStripButton";
-            this.formatDecToolStripButton.Size = new System.Drawing.Size(86, 22);
+            this.formatDecToolStripButton.Size = new System.Drawing.Size(49, 22);
             this.formatDecToolStripButton.Text = "&Format";
             this.formatDecToolStripButton.Click += new System.EventHandler(this.FormatDecToolStripButton_Click);
             // 
